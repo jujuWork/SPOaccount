fdede2a65b7941c8a9f4c62e6ef378eb5bcd565e
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-04 13:01:14 +0900

    inital commit

---
+++ themes/Accounts/pages/front.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sponago</title>
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
+
+</head>
+<body>
+    <div class="main-container">
+
+    </div>
+</body>
+</html>

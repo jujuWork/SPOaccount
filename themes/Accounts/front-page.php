f5ce24f2e51af024eb93e797b7798f5d58a4acc7
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-04 14:56:52 +0900

    test

--- themes/Accounts/front-page.php
+++ themes/Accounts/front-page.php
@@ -1,22 +1,25 @@
-<?php
-/**
- * Template Name: Front Page
- */
-get_header();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sponago</title>
 
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/top-page.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/footer.css">
+    
+    <?php wp_head();?>
+
 
 </head>
-<body>
-    <div class="main-container">
+<body <?php body_class(); ?>>
 
-    </div>
-</body>
-</html>
+        <h1>Accounts</h1>
+
+
+

62eaaf24748cd381c14c5a99c3f2c092c3c860e4
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-05 16:02:51 +0900

    front page

---
+++ themes/Accounts/front-page.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/top-page.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/footer.css">
+    
+    <?php wp_head();?>
+
+
+</head>
+<body <?php body_class(); ?>>
+
+        <h1>Accounts</h1>
+
+        
+
+

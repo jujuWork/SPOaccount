59e847ba31ff144deebf781686f76bb5a72f5579
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-10 11:54:32 +0900

    footer

--- themes/Accounts/footer.php
+++ themes/Accounts/footer.php
@@ -1,66 +1,142 @@
 <footer>
     <section class="main-container">
-        <div class="list-container">
-            <div class="list-item logo">
+        <div class="list-container one">
+            <div class="logo">
                 <h2>Sponago</h2>
             </div>
         </div>
-        <div class="list-container">
+        <div class="list-container two">
              <div class="list-item">
-                <p>
-                    <a href="#philosopy">スポNAGOの理念</a>
-                    <hr>
-                </p>
-                <p>
-                    <a href="#philosopy">運営法人情報</a>
-                </p>
+                <ul>
+                    <li class="list_title">スポNAGOの理念</li>
+                    <li>
+                        <a href="#information">運営法人情報</a>
+                    </li>
+                </ul>
             </div>
             <div class="list-item address">
-                <p>
-                    <a href="#address">所在地</a>
-                    <hr>
-                </p>
-                <p>〒450-0003</p>
-                <p>愛知県名古屋市中村区名駅南1丁目1-14</p>
-                <p>新納屋橋ビル5F</p>
+                <ul>
+                    <li class="list_title">所在地</li>
+                    <li>
+                        <a href="#address">
+                            <p>〒450-0003/p>
+                            <p>愛知県名古屋市中村区名駅南1丁目1-14</p>
+                            <p>新納屋橋ビル5F</p>
+                        </a>
+                    </li>
+                </ul>
             </div>
             <div class="list-item sponago_info">
-                <p>
-                    <a href="#contact">お問い合わせ</a>
-                    <hr>
-                </p>
-                                <p>
-                    <span><img src="<?php echo htmlspecialchars(get_template_directory_uri() . '/assets/icons/call.png') ?>" alt=""></span>
-                    052-561-0510
-                </p>
-                <p>
-                    <span><img src="<?php echo htmlspecialchars(get_template_directory_uri() . '/assets/icons/mail.png') ?>" alt=""></span>
-                    info@sportsmanship-nagoya.com
-                </p>
-                <p>
-                    <span class="font-blue">平日</span> 9:00 ~ 18:00 
-                    <span class="font-blue">土･祝 </span>11:00 ~ 15:00
-                </p>
+                <ul>
+                    <li class="list_title">お問い合わせ</li>
+                    <li>
+                        <img src="<?php echo htmlspecialchars(get_template_directory_uri() . '/assets/icons/call.png') ?>" alt="telephone">
+                        052-561-0510
+                    </li>
+                    <li>
+                        <img src="<?php echo htmlspecialchars(get_template_directory_uri() . '/assets/icons/mail.png') ?>" alt="mail">
+                        info@sportsmanship-nagoya.com
+                    </li>
+                    <li>
+                        <p>
+                            <span class="font-blue">平日</span> 9:00 ~ 18:00 
+                            <span class="font-blue">土･祝 </span>11:00 ~ 15:00
+                        </p>
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="list-container">
-            <div class="list-item logo office-list">
+        <div class="list-container three">
+            <div class="logo office-list">
                 <h2>事業所一覧</h2>
             </div>
         </div>
-        <div class="list-container">
+        <div class="list-container four">
             <div class="list-item">
-                <p>就労継続支援B型</p>
-                <hr>
+                <ul>
+                    <li class="list_title">就労継続支援B型</li>
+                    <li>
+                        <a href="#headquarter">本部(3･5F)</a>
+                    </li>
+                    <li>
+                        <a href="#yanagibashi">柳橋店</a>
+                    </li>
+                    <li>
+                        <a href="#nayabashi_curry">納屋橋カレーパン</a>
+                    </li>
+                    <li>
+                        <a href="#drone">ドローン</a>
+                    </li>
+                    <li>
+                        <a href="#green">GREEN</a>
+                    </li>
+                    <li>
+                        <a href="#parnasus">パルナス</a>
+                    </li>
+                </ul>
             </div>
-            <div class="list-item">2</div>
-            <div class="list-item">3</div>
-            <div class="list-item">4</div>
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">就労継続支援A型</li>
+                    <li>
+                        <a href="#agatta">就労継続支援A型</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">就労移行支援事業所</li>
+                    <li>
+                        <a href="#transition">就労移行支援事業所</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">地域活動支援</li>
+                    <li>
+                        <a href="#fitness">フィットネス</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="list-container">
-            <div class="list-item">1</div>
-            <div class="list-item">2</div>
-            <div class="list-item">3</div>
+        <div class="list-container five">
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">生活共同支援</li>
+                    <li>
+                        <a href="#residence01">レジデンス1</a>
+                    </li>
+                    <li>
+                        <a href="#residence02">レジデンス2</a>
+                    </li>
+                    <li>
+                        <a href="#residence03">レジデンス3</a>
+                    </li>
+                    <li>
+                        <a href="#residence04">レジデンス清須</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">生活介護</li>
+                    <li>
+                        <a href="#daily">生活介護</a>
+                    </li>
+                    <li>
+                        <a href="#daliy02">生活介護2</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <ul>
+                    <li class="list_title">訪問介護</li>
+                    <li>
+                        <a href="#nurse">名駅南訪問看護ステーション</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 </footer>

75e6008c933db8fef42f387ceed2b7c32b109f0b
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-12 16:45:16 +0900

    reupload

--- themes/Accounts/pages/a_gatta.php
+++ themes/Accounts/pages/a_gatta.php
@@ -18,15 +18,15 @@
         <div class="service__container-menu">
             <div class="service__container-items">
                 <img src="<?php echo htmlspecialchars(get_template_directory_uri() . '/images/a_gatta/web-development.png') ?>" alt="code">
-                <h3>Web Development</h3>
+                <h3>Web & Mobile Development</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <span>More</span>
             </div>
-            <div class="service__container-items">
+            <!-- <div class="service__container-items">
                 <h3>Mobile Development</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <span>More</span>
-            </div>
+            </div> -->
             <div class="service__container-items">
                 <h3>Designs</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
